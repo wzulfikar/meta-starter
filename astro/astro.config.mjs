9fe673cc5d28df2f02419104f23e7782810d498a
wzulfikar <>
2026-04-12 20:18:12 +0300

    add gh action to deploy static site at metastarter.wzulfikar.com

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  site: 'https://metastarter.wzulfikar.com',
   vite: {
     plugins: [tailwindcss()],
     server: {
